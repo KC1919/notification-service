2808a9086e6b4697262ea3ea7ba1b75b52f5b7f8
Kunal Chandra <kunal.chandra1900@gmail.com>
2025-03-01 00:56:17 +0530

    handle sms processing

--- server/service/sms_service.js
+++ server/service/sms_service.js
@@ -71,6 +71,7 @@ class SMSService {
 
     checkSMSQueue = async () => {
         try {
+            // if queue has any requests in it
             if (this.smsQueue.length > 0) {
                 if (this.isProcessing === false) {
                     await this.processSMSQueue();
@@ -99,35 +100,44 @@ class SMSService {
 
                 let eventsToProcess = smsQueueCopy.length;
 
-                this.isProcessing = true;
+                // if no processing of previous sms requests is being done
+                if (this.isProcessing === false) {
 
-                while (smsQueueCopy.length > 0 && this.isSMSServiceHealthy) {
-                    const data = smsQueueCopy[smsQueueCopy.length - 1];;
-                    const result = await this.sendSMSNotification(data);
-                    if (result === false) break;
-                    smsQueueCopy.pop();
-                }
+                    this.isProcessing = true;
 
-                if (this.isSMSServiceHealthy === false) {
-                    this.isProcessing = false;
-                }
-
-                if (smsQueueCopy.length < eventsToProcess || smsQueueCopy.length == 0) {
-                    this.isProcessing = false;
-
-                    this.smsQueue.reverse();
-
-                    let counter = eventsToProcess - smsQueueCopy.length;
-                    let copyCounter = counter;
-
-                    while (counter > 0) {
-                        this.smsQueue.pop();
-                        counter--;
+                    while (smsQueueCopy.length > 0 && this.isSMSServiceHealthy) {
+                        const data = smsQueueCopy[smsQueueCopy.length - 1];;
+                        const result = await this.sendSMSNotification(data);
+                        if (result === false) break;
+                        smsQueueCopy.pop();
                     }
 
-                    this.smsQueue.reverse();
+                    // if the service is down, stop the processing
+                    if (this.isSMSServiceHealthy === false) {
+                        this.isProcessing = false;
+                    }
 
-                    console.log('SMS Events processed:', copyCounter);
+                    if (smsQueueCopy.length < eventsToProcess || smsQueueCopy.length == 0) {
+                        this.isProcessing = false;
+
+                        // reversing the array to pop the message requests from the start
+                        this.smsQueue.reverse();
+
+                        // calculate the number of sms requests processed
+                        let counter = eventsToProcess - smsQueueCopy.length;
+                        let copyCounter = counter;
+
+                        // remove the messages processed from the queue
+                        while (counter > 0) {
+                            this.smsQueue.pop();
+                            counter--;
+                        }
+
+                        // again reverse the array to maintain the order of message requests(to make it act like a queue)
+                        this.smsQueue.reverse();
+
+                        console.log('SMS Events processed:', copyCounter);
+                    }
                 }
             }
 
@@ -142,6 +152,7 @@ class SMSService {
 
     startProcessing = async () => {
         try {
+            // check every 30 secs, if the service is healthy and if the queue has any requests to be processed
             setInterval(async () => {
                 const result = await this.checkSMSServiceHealth();
                 this.isSMSServiceHealthy = result;
